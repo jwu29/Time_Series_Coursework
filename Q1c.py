8db36eddb8576e724da9644c4726dd310d6e72f6
leahhh-308 <109014239+leahhh-308@users.noreply.github.com>
2023-12-05 13:39:36 +0000

    Update Q1c.py

--- Q1c.py
+++ Q1c.py
@@ -6,21 +6,16 @@ import matplotlib.pyplot as plt
 
 def periodogram(X):
     N = len(X)
-    X_N = X[N-1] # save last element of X
-    X.insert(0, 0) # pad X with 0 at the start
-    X.pop() # remove last element of X
     
     fft_X = fft.fft(X)
     fft_X = fft.fftshift(fft_X) # shift the transform to centre 0 frequency
-    
-    for i in range(N):
-        fft_X[i] += X_N # add the FT of last element back in
     
     ret = []
     for i in range(N):
         ret.append((1/N) * (fft_X[i].real**2 + fft_X[i].imag**2)) # find periodogram
     
     return ret
+    
 
 def direct(X, p):
     N = len(X)
@@ -41,17 +36,10 @@ def direct(X, p):
         else:
             h = C/2 * (1 - math.cos(2*np.pi*(N-t)/(pN+1)))
         Y.append(h * X[t])
-        
-    Y_N = Y[N-1] # save last element of Y
-    Y.insert(0, 0) # pad Y with 0 at the start
-    Y.pop() # remove last element of Y
     
     fft_Y = fft.fft(Y)
     fft_Y = fft.fftshift(fft_Y) # shift the transform to centre 0 frequency
-    
-    for i in range(N):
-        fft_Y[i] += Y_N # add the FT of last element back in
-    
+
     ret = []
     for i in range(N):
         ret.append(fft_Y[i].real**2 + fft_Y[i].imag**2) # find direct spectral estimate
