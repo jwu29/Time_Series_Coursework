8ed423583d892db58aedab565b18325537396fd5
jwu29 <93035014+jwu29@users.noreply.github.com>
2023-12-05 17:48:00 +0000

    Update Q1b.py
    Fixed implementation error.
    Line 8 - Change from 1/(abs(x)) --> 1/(abs(x)**2)
    Line 13 - Change from z.dot(phis) --> 1 - z.dot(phis)

--- Q1b.py
+++ Q1b.py
@@ -5,12 +5,12 @@ def EXP(y, f):
 EXP_2 = np.vectorize(EXP)
 
 def invabs(x):
-    return(1/(abs(x)))
+    return(1/(abs(x))**2)
 
 def S_AR(f,phis,sigma2):
     r = phis.size
     z = np.array([EXP_2(x, f) for x in range(1,r+1)]).T
-    result = sigma2 * np.array(list(map(invabs, z.dot(phis))))
+    result = sigma2 * np.array(list(map(invabs, 1 - z.dot(phis))))
     return(result)
 
 print(S_AR(np.array([-0.3,-0.1,0.2,0.4]),
